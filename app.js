bd5b2946b53e357aa708064e438872fefbed2be5
okSYS <ViacheslavDan803@gmail.com>
2023-10-25 23:24:36 +0300

    test end

--- app.js
+++ app.js
@@ -2,11 +2,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+// Указываем Express обслуживать статические файлы из текущей директории
+app.use(express.static(__dirname));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
